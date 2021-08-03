1d88b1c8789667d76e9cc2cbc35660fd1a22e180
xloz19 <tjwns53@naver.com>
2021-08-03 15:59:24 +0900

    day6 challenge modify

--- day6_challenge/app.js
+++ day6_challenge/app.js
@@ -1,51 +1,22 @@
-const generateForm = document.querySelector(".generate-number");
-const generateInput = generateForm.querySelector("input");
+const hidden = document.querySelector(".hidden");
+const hiddenH3 = hidden.querySelector("h3");
+const hiddenH5 = hidden.querySelector("h5");
+const guess = document.querySelector(".guess-number");
+const guessInput = guess.querySelector("input");
+const guessButton = guess.querySelector("button");
+const max = document.querySelector(".max-number");
+const maxInput = max.querySelector("input");
 
-const playForm = document.querySelector(".guess-number");
-const playInput = playForm.querySelector("input");
-const playButton = playForm.querySelector("Button");
-
-const result = document.querySelector("#choose");
-const winOrLost = document.querySelector("#win-lost");
-
-function onPlayBtnClick(event) {
-    event.preventDefault();
-    const playNumber = playInput.value;
-    const generateNumber = generateInput.value;
-    localStorage.setItem("generateNumber", generateNumber);
-    localStorage.setItem("playNumber", playNumber);
-    if ( playInput.value == 0 || generateInput.value == 0) {
-        alert("Please write number");
-    } else if ( playInput.value > generateInput.value ) {
-        alert("Please rewrite guess the number");
-    } else {
-        result.classList.remove("hidden");
-        winOrLost.classList.remove("hidden");
-    }
-    
+function guessButtonClick(event) {
+  event.preventDefault();
+  var randomNumber = Math.floor(Math.random() * maxInput.value) + 1;
+  hiddenH5.innerText =
+    "You chose: " + guessInput.value + ", the machine chose: " + randomNumber;
+  if (guessInput.value == randomNumber) {
+    hiddenH3.innerText = "You win!";
+  } else {
+    hiddenH3.innerText = "You lost!";
+  }
 }
 
-function randomChange() {
-    var randomNumber = Math.floor(Math.random()*generateInput.value)+1;
-    result.innerText = "You chose: " + playInput.value + ", the machine chose : " + randomNumber;
-    if ( randomNumber == playInput.value ) { 
-        winOrLost.innerText = "You win!"
-    } else { 
-        winOrLost.innerText = "You lose!"
-    }
-    
-}
-
-playButton.addEventListener("click", onPlayBtnClick);
-
-
-
-
-
-
-
-// 둘 다 숫자를 입력해야만 숨겨진 결과들이 나와야함 
-// 추측 숫자는 발생 최대 숫자보다 작야야함 
-// 랜덤숫자가 발생해야함
-// 랜덤숫자와 추측 숫자 크기 비교 
-// 숨겨진 글자에 윈 혹은 페일 나와야함 
+guessButton.addEventListener("click", guessButtonClick);
